961a7ecae2b816c92d5d5db7cb2006ff9c06d33f
succcx1 <1628477226@qq.com>
2024-02-21 00:00:27 +0800

    main

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<!-- <template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld></HelloWorld>
+  
+  <router-view />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+    
+  </template>
+  
+  
+  <style>
+  #app{
+    width:100%;
+    height:100%;
+    font-size:14px;
+    background:white
+  }
+  </style>
---
+++ src/views/index/index.vue
@@ -0,0 +1,31 @@
+<!-- <template>
+
+    <div>Index-page</div>
+
+</template>
+
+<script>
+
+export default {
+    name : 'Index-page',
+}
+
+
+</script>
+
+<style scoped>
+
+</style> -->
+<template>
+    <div class="index">主页 </div>
+      
+    </template>
+    
+<script>
+export default{
+    name:'Index-page'
+}
+</script>
+    <style>
+    
+    </style>
